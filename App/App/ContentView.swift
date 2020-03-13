43ae79e59272323552f47e9c1badf855f38a7933
hanif sugiyanto <m.hanif.sugiyantoE@accenture.com>
2020-03-14 06:36:41 +0700

    reinitialize

---
+++ App/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by M Hanif Sugiyanto on 14/03/20.
+//  Copyright © 2020 M Hanif Sugiyanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFOne/SFOne/SFOne.h
@@ -0,0 +1,19 @@
+//
+//  SFOne.h
+//  SFOne
+//
+//  Created by M Hanif Sugiyanto on 14/03/20.
+//  Copyright © 2020 M Hanif Sugiyanto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SFOne.
+FOUNDATION_EXPORT double SFOneVersionNumber;
+
+//! Project version string for SFOne.
+FOUNDATION_EXPORT const unsigned char SFOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFOne/PublicHeader.h>
+
+
